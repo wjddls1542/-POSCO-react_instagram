62d4f3beda1c8bd6de92030114d8ef11f3bb3450
wjddls1542 <wjddls1542@scnu.ac.kr>
2022-06-03 15:19:10 +0900

    context function delete->trimming step

--- src/App.js
+++ src/App.js
@@ -6,70 +6,25 @@ import BootstrapLogin from './components/Login/BootstrapLogin';
 import Page404 from './components/Page404';
 import Main from './components/Main';
 import Join from './components/Join/Join';
-import { useState } from 'react';
-import { UserContext } from './store/UserContext';
-import { Users } from './data/User';
 import Layout from './components/Layout/Layout';
 import Profile from './components/Profile/Profile';
-import { PostContext } from './store/PostContext';
-import { FollowContext } from './store/FollowContext';
-import { Post } from './data/Post';
-import { Follow } from './data/Follow';
+
 import Search from './components/Search/Search';
 function App() {
-   const [users, setUsers] = useState(Users);
-   const insertUsers = user => {
-      const newUser = { ...user, userId: user.id, id: users.length };
-      setUsers([...users, newUser]);
-   };
-   const updateUsers = user => {
-      const id = Number(localStorage.getItem('id'));
-      const { img, name } = user;
-      const findUsersIndex = users.findIndex(user => user.id === id);
-      if (findUsersIndex === -1) {
-         console.error('not found');
-         return;
-      }
-      const newUsers = [...users];
-      newUsers.splice(findUsersIndex, 1, { ...users[findUsersIndex], name, img }); //splice배열자르기
-      setUsers(newUsers);
-   };
-   const [posts, setPosts] = useState(Post);
-
-   const insertPost = post => {
-      const newPost = { ...post, userId: Number(localStorage.getItem('id')), id: posts.length };
-      setPosts([...posts, newPost]);
-   };
-
-   const deletePost = postId => {
-      const delPosts = posts.filter(post => post.id !== postId);
-      setPosts(delPosts);
-   };
-   const [follows, setFollows] = useState(Follow);
-   const insertFollow = followerId => {
-      const newFollow = { following: Number(localStorage.getItem('id')), follower: followerId };
-      setFollows([...follows, newFollow]);
-   };
    return (
-      <UserContext.Provider value={{ users, insertUsers, updateUsers }}>
-         <PostContext.Provider value={{ posts, insertPost, deletePost }}>
-            <FollowContext.Provider value={{ follows, insertFollow }}>
-               <BrowserRouter>
-                  <Routes>
-                     <Route path="/" element={<Layout></Layout>}>
-                        <Route index path="/" element={<Main></Main>}></Route>
-                        <Route path="shopping" element={<Main></Main>}></Route>
-                        <Route path="search" element={<Search></Search>}></Route>
-                        <Route path="profile" element={<Profile></Profile>}></Route>
-                     </Route>
-                     <Route index path="/login" element={<BootstrapLogin></BootstrapLogin>}></Route>
-                     <Route index path="/*" element={<Page404></Page404>}></Route>
-                     <Route index path="/join" element={<Join></Join>}></Route>
-                  </Routes>
-               </BrowserRouter>
-            </FollowContext.Provider>
-         </PostContext.Provider>
-      </UserContext.Provider>
+      <BrowserRouter>
+         <Routes>
+            <Route path="/" element={<Layout></Layout>}>
+               <Route index path="/" element={<Main></Main>}></Route>
+               <Route path="shopping" element={<Main></Main>}></Route>
+               <Route path="search" element={<Search></Search>}></Route>
+               <Route path="profile" element={<Profile></Profile>}></Route>
+            </Route>
+            <Route index path="/login" element={<BootstrapLogin></BootstrapLogin>}></Route>
+            <Route index path="/*" element={<Page404></Page404>}></Route>
+            <Route index path="/join" element={<Join></Join>}></Route>
+         </Routes>
+      </BrowserRouter>
    );
 }
 
--- src/components/Page404.js
+++ src/components/Page404.js
@@ -1,15 +1,12 @@
-import { useContext, useEffect } from 'react';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import { Users } from '../data/User';
-import { UserContext } from '../store/UserContext';
 
 const Page404 = () => {
    const navigate = useNavigate();
-   const { users } = useContext(UserContext);
+   const isLogin = useSelector(state => state.users.isLogin);
    useEffect(() => {
-      const id = localStorage.getItem('id');
-      const findUser = Users.find(data => data.id === Number(id));
-      if (!findUser) {
+      if (!isLogin) {
          navigate('/login');
       } else {
          navigate('/');
